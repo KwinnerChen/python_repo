e6cc4c6d042a8dd09271b37d52735d4a0645472b
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-06 16:44:16 +0800

    update:添加递归实现

--- python_algorithm/choicesort.py
+++ python_algorithm/choicesort.py
@@ -3,13 +3,15 @@
 
 
 """
-不是协程练习，选择排序算法的实现
+选择排序算法的实现
+将每个元素和其后元素进行比较，是否更换位置
 """
 
 
 from run_test import main
 
 
+# 迭代实现
 def ch_que(query):
     for i in range(len(query)-1):
         for j in range(i+1, len(query)):
@@ -18,7 +20,20 @@ def ch_que(query):
     return query
 
 
+# 递归实现
+def ch_que_rec(seq, i):
+    if i == 0:
+        return
+    j_max = i
+    for j in range(i):
+        if seq[j] > seq[j_max]:
+            j_max = j
+    seq[i], seq[j_max] = seq[j_max], seq[i]
+    ch_que_rec(seq, i-1)
+    return seq
+
+
 if __name__ == '__main__':
-    dtime = main(ch_que, seq_len=10000)
+    dtime = main(ch_que_rec)
     print(f"耗时: {dtime}s")
     
