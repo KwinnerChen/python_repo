45a7f9d12daffdef739482d0339fa25d06de5f97
KwinnerChen <chenkai.c.k@hotmail.com>
2021-08-08 20:23:59 +0800

    add:asyncio training

---
+++ asyncio_training/echo_client.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+module for testing echo server
+"""
+
+
+import asyncio
+from asyncio import tasks
+
+
+async def echo_client(message: str):
+    reader, writer = await asyncio.open_connection("localhost", 8080)
+
+    print(f"send message: {message}")
+    writer.write(message.encode())
+    await writer.drain()
+
+    data = await reader.read(100)
+    message = data.decode()
+    print(f"recieved message: {message}")
+
+    print("close the connection")
+    writer.close()
+
+
+async def test():
+    tasks = []
+    for i in range(10000):
+        task = echo_client(f"hello num {i}")
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(test())
---
+++ asyncio_training/echo_server.py
@@ -0,0 +1,45 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+a simple echo server
+"""
+
+
+import asyncio
+import random
+
+
+async def handle(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    data = await reader.read(100)
+    message = data.decode()
+
+    addr = writer.get_extra_info("peername")
+    print(f"recieved {message!r} from {addr!r}")
+
+    await asyncio.sleep(random.randint(2, 10))
+    print(f"send: {message!r}")
+    writer.write(message.encode())
+    await writer.drain()
+
+    print("close the connection")
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(
+        handle,
+        'localhost',
+        8080
+    )
+
+    addr = server.sockets[0].getsockname()
+    print(f"servering on {addr}")
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
