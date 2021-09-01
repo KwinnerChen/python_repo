469e2ea6cc38e0c2278969ee8d1f416da4eb6454
KwinnerChen <chenkai.c.k@hotmail.com>
2021-09-01 11:21:28 +0800

    update:修改下载重试逻辑

--- xiaoqiang/base/downloader.py
+++ xiaoqiang/base/downloader.py
@@ -27,20 +27,34 @@ class Donlowder:
         self.session = requests.Session() if session else requests
         self.max_retry = max_retry
         self.mdws = mdws
-        self.__retry_times_map = defaultdict(int)
 
     def get_page(self, task):
-        task_hash = hash(task)
-        assert self.__check_task(task, task_hash), "重试已超过限定次数！"
+        if self.max_retry < 0:
+            while True:
+                resp, err = self.__get_page__inner(task)
+                if err is None:
+                    return resp, None
+        
+        elif self.max_retry == 0:
+            return self.__get_page__inner(task)
+        
+        else:
+            for _ in range(self.max_retry):
+                resp, err = self.__get_page__inner(task)
+                if err is None:
+                    return resp, None
+            return resp, err
+        
+
+    def __get_page__inner(self, task):
         task = self.__mdws_process(task)
-        method = task.method
 
-        if method.low() == 'get':
-            return self.get(task, task_hash)
-        elif method.low() == 'post':
-            return self.post(task, task_hash)
+        if task.method.lower() == 'get':
+            return self.get(task)
+        elif task.method.lower() == 'post':
+            return self.post(task)
 
-    def get(self, task:Task, task_hash):
+    def get(self, task:Task):
         try:
             resp = self.session.get(url=task.url, **task.kwargs)
             resp.raise_for_status()
@@ -48,10 +62,9 @@ class Donlowder:
                 resp.delayitem = task.delayitem
             return resp, None
         except Exception as e:
-            self.__retry_times_map[task_hash] += 1
             return None, e
         
-    def post(self, task:Task, task_hash):
+    def post(self, task:Task):
         try:
             resp = self.session.post(url=task.url, **task.kwargs)
             resp.raise_for_status()
@@ -59,23 +72,9 @@ class Donlowder:
                 resp.delayitem = task.delayitem
             return resp, None
         except Exception as e:
-            self.__retry_times_map[task_hash] += 1
             return None, e
 
     def __mdws_process(self, task):
         for mdw in self.mdws:
             task = mdw.process(task)
         return task
-                
-    def __check_task(self, task:Task, task_hash):
-        if task.times == 0:
-            if self.__retry_times_map[task_hash] <= self.max_retry:
-                return True
-            else:
-                return False
-        else:
-            if self.__retry_times_map[task_hash] <= task.times:
-                return True
-            else:
-                return False
-
