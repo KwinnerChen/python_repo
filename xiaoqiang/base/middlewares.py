5ce0c1025c5fd6099345c767bead9e628e687587
KwinnerChen <chenkai.c.k@hotmail.com>
2021-09-01 11:24:02 +0800

    update:修复抽象基类的接口错误

--- xiaoqiang/base/middlewares.py
+++ xiaoqiang/base/middlewares.py
@@ -15,13 +15,14 @@ class MiddleWare(ABC):
         """
         参数必须是Task对象，且最终返回值也必须是Task对象。
         """
-        assert isinstance(task, Task) or task is None, f"请求中间件{self.__class__}只能接受任务对象或者None为参数！"
+        assert isinstance(task, Task), f"请求中间件{self.__class__.__name__}只能接受任务对象为参数！"
         task = self.task_process(task)
-        assert isinstance(task, Task) or task is None, f"请求中间件{self.__class__}只能返回一个任务对象或者None！"
+        assert isinstance(task, Task) or task is None, f"请求中间件{self.__class__.__name__}只能返回一个任务对象或者None！"
+        return task
 
     @abstractmethod
     def task_process(self, task):
         """
         参数为Task对象，返回值也必须时Task对象。
         """
-        pass
+        return task
